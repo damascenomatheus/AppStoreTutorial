d7b2ed8b88cc1c18f8d0c4201d102938884e2d9a
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-09-06 14:42:46 -0300

    Setting rootViewController

---
+++ AppStoreFake/BaseTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreFake
+//
+//  Created by Matheus Damasceno on 06/09/19.
+//  Copyright © 2019 Matheus Damasceno. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+}
