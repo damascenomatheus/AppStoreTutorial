b8ab5f357256611ae5afa3d07ad4b9d8b903aa8d
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-09-20 09:02:55 -0300

    Implementing TodayViewController

--- AppStoreFake/BaseTabBarController.swift
+++ AppStoreFake/BaseTabBarController.swift
@@ -13,6 +13,10 @@ class BaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let todayViewController = UIViewController()
+        todayViewController.navigationItem.title = "Today"
+        todayViewController.view.backgroundColor = .white
+        
         let appsViewController = UIViewController()
         appsViewController.view.backgroundColor = .white
         appsViewController.navigationItem.title = "Apps"
@@ -23,6 +27,8 @@ class BaseTabBarController: UITabBarController {
         
         let appNavController = UINavigationController(rootViewController: appsViewController)
         let searchNavController = UINavigationController(rootViewController: searchViewController)
+        let todayNavController = UINavigationController(rootViewController: todayViewController)
+        
         
         appNavController.tabBarItem.title = "Apps"
         appNavController.tabBarItem.image = UIImage(named: "apps")
@@ -30,9 +36,12 @@ class BaseTabBarController: UITabBarController {
         searchNavController.tabBarItem.title = "Search"
         searchNavController.tabBarItem.image = UIImage(named: "search")
         searchNavController.navigationBar.prefersLargeTitles = true
-        
+        todayNavController.navigationBar.prefersLargeTitles = true
+        todayNavController.tabBarItem.image = UIImage(named: "today_icon")
+        todayNavController.tabBarItem.title = "Today"
         
         viewControllers = [
+            todayNavController,
             appNavController,
             searchNavController
         ]
