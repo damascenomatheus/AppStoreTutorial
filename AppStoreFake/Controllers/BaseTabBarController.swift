5585c9b57aa854e4a0aaebd1d9df41cb79d96f8e
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-09-23 10:09:48 -0300

    Refactoring NavControllers

---
+++ AppStoreFake/Controllers/BaseTabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreFake
+//
+//  Created by Matheus Damasceno on 06/09/19.
+//  Copyright © 2019 Matheus Damasceno. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", image: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", image: "apps"),
+            createNavController(viewController: UIViewController(), title: "Search", image: "search")
+        ]
+        
+    }
+    
+    func createNavController(viewController: UIViewController, title: String, image: String) -> UINavigationController {
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .white
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: image)
+        return navController
+    }
+}
