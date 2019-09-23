0d0b2646e4d2cb5575b098e60f1195dce7951e28
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-09-23 10:54:00 -0300

    Implementing Search CollectionController

---
+++ AppStoreFake/Controllers/AppSearchController.swift
@@ -0,0 +1,50 @@
+//
+//  AppSearchController.swift
+//  AppStoreFake
+//
+//  Created by Matheus Damasceno on 23/09/19.
+//  Copyright © 2019 Matheus Damasceno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    override init(collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Test")
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Test", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: view.frame.height/3)
+    }
+    
+    
+    
+    
+    
+}
