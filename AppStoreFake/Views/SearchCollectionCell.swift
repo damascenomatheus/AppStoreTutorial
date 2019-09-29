ca6c69a75b3a22ecbaa7d574e4fda46be9664034
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-09-29 17:14:36 -0300

    Fixing stackView Size

--- AppStoreFake/Views/SearchCollectionCell.swift
+++ AppStoreFake/Views/SearchCollectionCell.swift
@@ -14,7 +14,6 @@ class SearchCollectionCell: UICollectionViewCell {
         let imageView = UIImageView()
         imageView.backgroundColor = .blue
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.layer.cornerRadius = 10
         return imageView
     }()
     
@@ -22,7 +21,7 @@ class SearchCollectionCell: UICollectionViewCell {
     let categoryLabel = UILabel(title: "Photos & Video")
     let ratingsLabel = UILabel(title: "9.26M")
     let getButton = CustomButton(type: .system)
-    let stackView = UIStackView()
+    let horizontalStackView = UIStackView()
         
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -36,45 +35,45 @@ class SearchCollectionCell: UICollectionViewCell {
     }
     
     func setupStackView() {
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(stackView)
-        stackView.addArrangedSubviews([
+        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
+        horizontalStackView.spacing = 12
+        horizontalStackView.addArrangedSubviews([
             appImage,
             appTitle,
-            categoryLabel,
-            ratingsLabel,
+            //categoryLabel,
+            //ratingsLabel,
             getButton
         ])
         
+        addSubview(horizontalStackView)
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            horizontalStackView.heightAnchor.constraint(equalToConstant: 50),
+            horizontalStackView.widthAnchor.constraint(equalToConstant: frame.width)
         ])
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            appImage.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
-            appImage.heightAnchor.constraint(equalToConstant: 44),
-            appImage.widthAnchor.constraint(equalToConstant: 44),
-            appImage.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 16),
-        
-            appTitle.topAnchor.constraint(equalTo: appImage.topAnchor),
-            appTitle.widthAnchor.constraint(equalToConstant: self.frame.width),
-            appTitle.heightAnchor.constraint(equalToConstant: 20),
-            appTitle.leadingAnchor.constraint(equalTo: appImage.trailingAnchor, constant: 8),
-        
-            categoryLabel.topAnchor.constraint(equalTo: appTitle.bottomAnchor, constant: 4),
-            categoryLabel.leadingAnchor.constraint(equalTo: appImage.trailingAnchor, constant: 8),
-            categoryLabel.widthAnchor.constraint(equalToConstant: self.frame.width),
-            categoryLabel.heightAnchor.constraint(equalToConstant: 20),
             
-            getButton.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
-            getButton.heightAnchor.constraint(equalToConstant: 20),
-            getButton.widthAnchor.constraint(equalToConstant: 50),
-            getButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -16)
+//            appImage.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
+//            appImage.heightAnchor.constraint(equalToConstant: 44),
+            appImage.widthAnchor.constraint(equalToConstant: 64),
+//            appImage.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 16),
+//
+//            appTitle.topAnchor.constraint(equalTo: appImage.topAnchor),
+//            appTitle.widthAnchor.constraint(equalToConstant: self.frame.width),
+//            appTitle.heightAnchor.constraint(equalToConstant: 20),
+//            appTitle.leadingAnchor.constraint(equalTo: appImage.trailingAnchor, constant: 8),
+//
+//            categoryLabel.topAnchor.constraint(equalTo: appTitle.bottomAnchor, constant: 4),
+//            categoryLabel.leadingAnchor.constraint(equalTo: appImage.trailingAnchor, constant: 8),
+//            categoryLabel.widthAnchor.constraint(equalToConstant: self.frame.width),
+//            categoryLabel.heightAnchor.constraint(equalToConstant: 20),
+//
+//            getButton.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
+//            getButton.heightAnchor.constraint(equalToConstant: 20),
+            getButton.widthAnchor.constraint(equalToConstant: 80),
+//            getButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -16)
         ])
     }
     
