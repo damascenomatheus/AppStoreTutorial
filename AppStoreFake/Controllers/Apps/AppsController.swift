8c01af47353142ce353e5cf9ca5ec370f3cc32db
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-10-29 09:21:26 -0300

    Implementing CollectionView layout

--- AppStoreFake/Controllers/Apps/AppsController.swift
+++ AppStoreFake/Controllers/Apps/AppsController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class AppsController: UICollectionViewController {
     
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .yellow
     }
+    
+    
 }
