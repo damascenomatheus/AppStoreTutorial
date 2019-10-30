fef7446eecd42c11e8986a0e9cf00884de26969e
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-10-30 09:11:19 -0300

    Update Horizontal Collection Controller

--- AppStoreFake/Controllers/Apps/AppsHorizontalController.swift
+++ AppStoreFake/Controllers/Apps/AppsHorizontalController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class AppsHorizontalController: BaseCollectionController {
     
+    fileprivate let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .purple
+        setupCollection()
+    }
+    
+    fileprivate func setupCollection() {
+        collectionView.backgroundColor = .blue
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
     }
 }
