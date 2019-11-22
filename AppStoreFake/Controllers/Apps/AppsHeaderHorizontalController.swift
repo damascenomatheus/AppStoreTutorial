707847a75eaa62f9eb0b03d491658c7c60494622
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-11-22 10:03:33 -0300

    Implemented HeaderHorizontal Collection

--- AppStoreFake/Controllers/Apps/AppsHeaderHorizontalController.swift
+++ AppStoreFake/Controllers/Apps/AppsHeaderHorizontalController.swift
@@ -9,8 +9,27 @@
 import UIKit
 
 class AppsHeaderHorizontalController: BaseCollectionController {
+    
+    fileprivate let cellId = "cellId"
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .yellow
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+}
+
+extension AppsHeaderHorizontalController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 300)
     }
 }
