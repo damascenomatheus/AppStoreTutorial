2096c0659561aa765a70d03833f7baed1c85b63a
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-11-22 09:21:14 -0300

    Implemented nested header ControllerView

--- AppStoreFake/Views/Apps/AppsPageHeader.swift
+++ AppStoreFake/Views/Apps/AppsPageHeader.swift
@@ -9,13 +9,33 @@
 import UIKit
 
 class AppsPageHeader: UICollectionReusableView {
+    
+    let controller = UIViewController()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .blue
+        controller.view.backgroundColor = .purple
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        buildViewHierarchy()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func buildViewHierarchy() {
+        addSubviews([controller.view])
+    }
+    
+    fileprivate func setupConstraints() {
+        NSLayoutConstraint.activate([
+            controller.view.topAnchor.constraint(equalTo: self.topAnchor),
+            controller.view.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            controller.view.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            controller.view.bottomAnchor.constraint(equalTo:  self.bottomAnchor)
+        
+        ])
     }
     
     
