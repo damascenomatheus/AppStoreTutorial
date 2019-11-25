58327654b89f34c8d4549479936bc0941df9c8d8
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-11-25 08:38:43 -0300

    changing cell height

--- AppStoreFake/Controllers/Apps/AppsHeaderHorizontalController.swift
+++ AppStoreFake/Controllers/Apps/AppsHeaderHorizontalController.swift
@@ -36,7 +36,7 @@ class AppsHeaderHorizontalController: BaseCollectionController {
 
 extension AppsHeaderHorizontalController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width, height: 300)
+        return .init(width: view.frame.width, height: view.frame.height)
     }
 }
 
