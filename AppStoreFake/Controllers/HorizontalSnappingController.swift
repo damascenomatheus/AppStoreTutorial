9459cbcc9a0a2ac004013738dfa913e639ac31cf
Matheus <matheusdamascenotx@gmail.com>
2020-02-25 23:09:35 -0300

    creating horizontal snapping controller

---
+++ AppStoreFake/Controllers/HorizontalSnappingController.swift
@@ -0,0 +1,20 @@
+//
+//  HorizontalSnappingController.swift
+//  AppStoreFake
+//
+//  Created by Matheus Damasceno on 25/02/20.
+//  Copyright © 2020 Matheus Damasceno. All rights reserved.
+//
+
+import UIKit
+
+class HorizontalSnappingController: UICollectionViewController {
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
