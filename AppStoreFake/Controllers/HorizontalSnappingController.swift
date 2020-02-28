31abf1f07afc8483ed3d0d98bad68fc0c9577802
Matheus <matheusdamascenotx@gmail.com>
2020-02-27 21:40:28 -0300

    adding method to snap cell in collection view

--- AppStoreFake/Controllers/HorizontalSnappingController.swift
+++ AppStoreFake/Controllers/HorizontalSnappingController.swift
@@ -25,6 +25,23 @@ class HorizontalSnappingController: UICollectionViewController {
 class SnappingLayout: UICollectionViewFlowLayout {
     
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-        return .zero
+      guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
+      let parent = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+
+      let itemSpace = itemSize.width + minimumInteritemSpacing
+      var currentItemIdx = round(collectionView.contentOffset.x / itemSpace)
+
+      // Skip to the next cell, if there is residual scrolling velocity left.
+      // This helps to prevent glitches
+      let vX = velocity.x
+      if vX > 0 {
+        currentItemIdx += 1
+      } else if vX < 0 {
+        currentItemIdx -= 1
+      }
+
+      let nearestPageOffset = currentItemIdx * itemSpace
+      return CGPoint(x: nearestPageOffset,
+                     y: parent.y)
     }
 }
