c497938bd506fc4fc542940af54089065c556169
Matheus <matheusdamascenotx@gmail.com>
2020-02-27 21:43:01 -0300

    change item size

--- AppStoreFake/Controllers/HorizontalSnappingController.swift
+++ AppStoreFake/Controllers/HorizontalSnappingController.swift
@@ -25,23 +25,24 @@ class HorizontalSnappingController: UICollectionViewController {
 class SnappingLayout: UICollectionViewFlowLayout {
     
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-      guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
-      let parent = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+        guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
+        let parent = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+            
+        let item = itemSize.width - 32
+        let itemSpace = item + minimumInteritemSpacing
+        var currentItemIdx = round(collectionView.contentOffset.x / itemSpace)
 
-      let itemSpace = itemSize.width + minimumInteritemSpacing
-      var currentItemIdx = round(collectionView.contentOffset.x / itemSpace)
+          // Skip to the next cell, if there is residual scrolling velocity left.
+          // This helps to prevent glitches
+        let vX = velocity.x
+        if vX > 0 {
+            currentItemIdx += 1
+        } else if vX < 0 {
+            currentItemIdx -= 1
+        }
 
-      // Skip to the next cell, if there is residual scrolling velocity left.
-      // This helps to prevent glitches
-      let vX = velocity.x
-      if vX > 0 {
-        currentItemIdx += 1
-      } else if vX < 0 {
-        currentItemIdx -= 1
-      }
+        let nearestPageOffset = currentItemIdx * itemSpace
+        return CGPoint(x: nearestPageOffset, y: parent.y)
 
-      let nearestPageOffset = currentItemIdx * itemSpace
-      return CGPoint(x: nearestPageOffset,
-                     y: parent.y)
     }
 }
